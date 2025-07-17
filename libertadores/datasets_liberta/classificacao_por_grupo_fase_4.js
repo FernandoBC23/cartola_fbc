eb0d1be6a4eeeca13e3f4793b4a8498f918f96db
Fernando Chagas <fernandobchagas@gmail.com>
2025-07-17 12:49:27 -0300

    Ajuste Libertadores
    Criando as semifinais da Libertadores

---
+++ libertadores/datasets_liberta/classificacao_por_grupo_fase_4.js
@@ -0,0 +1,50 @@
+const classificacaoFase4 = {
+  "Jogo 1 (JG1)": [
+    {
+      "posicao": 1,
+      "nome": "Real SCI",
+      "pontos": 3,
+      "vitorias": 1,
+      "empates": 0,
+      "derrotas": 1,
+      "totalCartola": 193.419921875,
+      "cartolaSofrido": 188.650390625,
+      "saldoCartola": 4.76953125
+    },
+    {
+      "posicao": 2,
+      "nome": "Analove10 ITAQUI GRANDE!!",
+      "pontos": 3,
+      "vitorias": 1,
+      "empates": 0,
+      "derrotas": 1,
+      "totalCartola": 188.650390625,
+      "cartolaSofrido": 193.419921875,
+      "saldoCartola": -4.76953125
+    }
+  ],
+  "Jogo 2 (JG2)": [
+    {
+      "posicao": 1,
+      "nome": "Lá do Itaqui",
+      "pontos": 3,
+      "vitorias": 1,
+      "empates": 0,
+      "derrotas": 1,
+      "totalCartola": 187.08984375,
+      "cartolaSofrido": 181.169921875,
+      "saldoCartola": 5.919921875
+    },
+    {
+      "posicao": 2,
+      "nome": "Texas Club 2025",
+      "pontos": 3,
+      "vitorias": 1,
+      "empates": 0,
+      "derrotas": 1,
+      "totalCartola": 181.169921875,
+      "cartolaSofrido": 187.08984375,
+      "saldoCartola": -5.919921875
+    }
+  ]
+};
---
+++ libertadores/datasets_liberta/confrontos_fase4_libertadores.js
@@ -0,0 +1,1 @@
+const confrontosFase4 = [];
