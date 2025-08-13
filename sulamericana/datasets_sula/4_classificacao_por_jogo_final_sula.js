7b7e403500a3d8ff73829b32af12368f0d968807
Fernando Chagas <fernandobchagas@gmail.com>
2025-08-12 23:02:18 -0300

    Atualização Final SULA

--- sulamericana/datasets_sula/4_classificacao_por_jogo_final_sula.js
+++ sulamericana/datasets_sula/4_classificacao_por_jogo_final_sula.js
@@ -1,50 +1,50 @@
 const classificacao_final_sula = {
-  "Final": [
-    {
-      "posicao": 0,
-      "nome": "Grêmio imortal 37",
-      "pontos": 0,
-      "vitorias": 0,
-      "empates": 0,
-      "derrotas": 0,
-      "totalCartola": 0.0,
-      "cartolaSofrido": 0.0,
-      "saldoCartola": 0.0
-    },
-    {
-      "posicao": 0,
-      "nome": "MauHumor F.C.",
-      "pontos": 0,
-      "vitorias": 0,
-      "empates": 0,
-      "derrotas": 0,
-      "totalCartola": 0.0,
-      "cartolaSofrido": 0.0,
-      "saldoCartola": 0.0
-    }
-  ],
   "Decisão 3º Lugar": [
     {
-      "posicao": 0,
+      "posicao": 1,
       "nome": "KING LEONN",
-      "pontos": 0,
-      "vitorias": 0,
+      "pontos": 3,
+      "vitorias": 1,
       "empates": 0,
       "derrotas": 0,
-      "totalCartola": 0.0,
-      "cartolaSofrido": 0.0,
-      "saldoCartola": 0.0
+      "totalCartola": 64.2998046875,
+      "cartolaSofrido": 52.0,
+      "saldoCartola": 12.2998046875
     },
     {
-      "posicao": 0,
+      "posicao": 2,
       "nome": "lsauer fc",
       "pontos": 0,
       "vitorias": 0,
       "empates": 0,
+      "derrotas": 1,
+      "totalCartola": 52.0,
+      "cartolaSofrido": 64.2998046875,
+      "saldoCartola": -12.2998046875
+    }
+  ],
+  "Final": [
+    {
+      "posicao": 1,
+      "nome": "MauHumor F.C.",
+      "pontos": 3,
+      "vitorias": 1,
+      "empates": 0,
       "derrotas": 0,
-      "totalCartola": 0.0,
-      "cartolaSofrido": 0.0,
-      "saldoCartola": 0.0
+      "totalCartola": 66.7001953125,
+      "cartolaSofrido": 62.949951171875,
+      "saldoCartola": 3.750244140625
+    },
+    {
+      "posicao": 2,
+      "nome": "Grêmio imortal 37",
+      "pontos": 0,
+      "vitorias": 0,
+      "empates": 0,
+      "derrotas": 1,
+      "totalCartola": 62.949951171875,
+      "cartolaSofrido": 66.7001953125,
+      "saldoCartola": -3.750244140625
     }
   ]
 };
